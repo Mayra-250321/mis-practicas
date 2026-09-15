120c5f39603ad1e50b90341ec3c21ff0dc13f0ea
Mayra-250321 <250321@utxicotepec.edu.mx>
2026-09-15 11:06:31 -0600

    Ejemplo de big O arreglo.py

---
+++ Arreglo.py
@@ -0,0 +1,4 @@
+# O(1): Acceso y adición rápida
+mi_arreglo = [10, 20, 30, 40]
+print(mi_arreglo[2])  # Acceso directo al índice 2 -> Muestra 30
+mi_arreglo.append(50)  # Agrega al final de forma eficiente
