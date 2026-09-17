8467a908e6c0b504f04690ab2dac24fe51663a21
Mayra-250321 <250321@utxicotepec.edu.mx>
2026-09-17 13:57:30 -0600

    Add files via upload

---
+++ bublesort.py
@@ -0,0 +1,10 @@
+lista=[10, 40, 15, 100]
+n = len(lista)
+swapped = True
+while swapped: 
+    swapped=False 
+for i in range(n-1):
+    if lista[i]>lista[i+1]:
+        lista[i], lista[i+1]=lista[i+1], lista[i]
+        swapped=True
+print("lista ordenada:", lista)
